ebb38d4990cf7d38683d0b1d5e88d4b923f9683b
AurionLab <aurion@student.telkomuniversity.ac.id>
2019-11-14 12:42:00 +0700

    Updates

---
+++ precache-manifest.aa2f5d6b8f8a714c35538e86c15b7d66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcce410ad1a1d2fc9e6819ff7a3146ee",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c13e96962e3a94186705",
+    "url": "/monsters-rolodex/static/css/main.6368ca33.chunk.css"
+  },
+  {
+    "revision": "c6214f979b742b0bcadf",
+    "url": "/monsters-rolodex/static/js/2.f65d83a6.chunk.js"
+  },
+  {
+    "revision": "c13e96962e3a94186705",
+    "url": "/monsters-rolodex/static/js/main.aad2630e.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
